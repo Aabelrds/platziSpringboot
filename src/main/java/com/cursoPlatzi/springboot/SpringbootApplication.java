154c051d0a1632743230d9ba2c425424e6252f62
A Rodrigues Da Silva <alejandro-abel.rodrigues-da-silva@capgemini.com>
2022-01-13 16:06:47 +0100

    log implemented  and  variables implement  in the properties file

--- src/main/java/com/cursoPlatzi/springboot/SpringbootApplication.java
+++ src/main/java/com/cursoPlatzi/springboot/SpringbootApplication.java
@@ -1,5 +1,7 @@
 package com.cursoPlatzi.springboot;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,13 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.cursoPlatzi.springboot.bean.MyBean;
 import com.cursoPlatzi.springboot.bean.MyBeanProperties;
 import com.cursoPlatzi.springboot.bean.MyBeanWithDependency;
-import com.cursoPlatzi.springboot.bean.MyOperation;
 import com.cursoPlatzi.springboot.component.ComponentDependency;
 import com.cursoPlatzi.springboot.pojo.UserPojo;
 
 @SpringBootApplication
 public class SpringbootApplication implements CommandLineRunner{
 	
+	Log LOGGER = LogFactory.getLog(SpringbootApplication.class);
 	// Inyectamos las dependencias en el constructor de la clase principal 
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
@@ -49,11 +51,20 @@ public class SpringbootApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args){
 		
-		this.componentDependency.sayHello();
-		this.myBean.print();
+//		this.componentDependency.sayHello();
+//		this.myBean.print();
 		this.myBeanWithDependency.printWithDependency();
-		System.out.println(this.myBeanProperties.getValues());
-		System.out.println(this.userPojo.getEmail());
+//		System.out.println(this.myBeanProperties.getValues());
+		System.out.println(this.userPojo.getEmail()+" POJO EXAMPLE WITH PROPERTIES VALUE");
+		//Con esto podemos debugear la app (ejemplo)
+		try {
+			// generando error
+			int value = 10/0;
+			LOGGER.debug("Mi valor generado con error es :" + " " +value);
+		} catch (Exception e) {
+			
+			LOGGER.error("Debugging error : " + e.getMessage());
+		}
 		
 	}
 
--- src/main/java/com/cursoPlatzi/springboot/bean/MyBeanWithDependencyImpl.java
+++ src/main/java/com/cursoPlatzi/springboot/bean/MyBeanWithDependencyImpl.java
@@ -1,7 +1,11 @@
 package com.cursoPlatzi.springboot.bean;
 
-public class MyBeanWithDependencyImpl implements MyBeanWithDependency{
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
+public class MyBeanWithDependencyImpl implements MyBeanWithDependency{
+	
+	Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImpl.class);
 	private MyOperation myOperation;
 	
 	public MyBeanWithDependencyImpl(MyOperation myOperation) {
@@ -12,7 +16,10 @@ public class MyBeanWithDependencyImpl implements MyBeanWithDependency{
 	@Override
 	public void printWithDependency() {
 		
+		// Nos sirve para debuggear lo que pasa en determinada parte de la app
+		LOGGER.info("Hemos ingresado al metodo MyBeanWaithDependency");
 		int numero = 2;
+		LOGGER.debug("El número enviado para la operación es :" + numero);
 		System.out.println("Hellow desde MyBeanWithDependency ");
 		System.out.println(this.myOperation.sum(numero));
 		
